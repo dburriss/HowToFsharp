ae278369409279029f80ac94db0536df21d5e95e
dburriss <devon@chimplab.co>
2018-11-20 06:59:17 +0100

    Prints Menu

--- Program.fs
+++ Program.fs
@@ -27,8 +27,33 @@ let rec captureContacts saveContact =
         captureContacts saveContact
     | Choice2Of2 _ -> ()
 
+let printMenu() =
+    Console.WriteLine("====================")
+    Console.WriteLine("MENU")
+    Console.WriteLine("====================")
+    Console.WriteLine("1. Print Contacts")
+    Console.WriteLine("2. Capture Contacts")
+    Console.WriteLine("0. Quit")
+
+let saveContact contact = printfn "%A" contact
+
+let routeMenuOption i =
+    match i with
+    | "1" -> printfn "Contacts"
+    | "2" -> captureContacts saveContact
+    | _ -> printMenu()
+
+let readKey() =
+    let k = Console.ReadKey()
+    Console.WriteLine()
+    k.KeyChar |> string
+
 [<EntryPoint>]
 let main argv =
-    let save c = printfn "%A" c
-    captureContacts save
+    printMenu()
+    let mutable selection = readKey()
+    while(selection <> "0") do
+        routeMenuOption selection
+        printMenu()
+        selection <- readKey()
     0
