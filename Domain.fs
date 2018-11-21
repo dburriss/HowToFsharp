21efdcd9a332bfe07699eb597a06999e7a233ea5
dburriss <devon@chimplab.co>
2018-11-21 07:03:05 +0100

    Adds validation function

--- Domain.fs
+++ Domain.fs
@@ -8,3 +8,21 @@ type Contact = {
     Lastname:string
     Email:string
 }
+
+module Contact =
+    let private isValidEmail (email:string) =
+        try
+            new System.Net.Mail.MailAddress(email) |> ignore
+            true
+        with 
+        | _ -> false
+
+    let validate contact =
+        let errors = seq {
+            if(String.IsNullOrEmpty(contact.Firstname)) then yield "First name should not be empty"
+            if(String.IsNullOrEmpty(contact.Lastname)) then yield "Last name should not be empty"
+            if(String.IsNullOrEmpty(contact.Email)) then yield "Email should not be empty"
+            if(isValidEmail contact.Email |> not) then yield "Not a valid email"
+        }
+
+        if(Seq.isEmpty errors) then Ok contact else Error errors
