44bc26e0623e2536c66dc46adafd6845909a06dc
dburriss <devon@chimplab.co>
2018-11-21 22:13:40 +0100

    Entity for guid to string

--- Data.fs
+++ Data.fs
@@ -1,8 +1,11 @@
 namespace Contacts
 
+open Contacts
 open System.Data.SQLite
 
 module Data =
+
+    type ContactEntity = { id:string; firstname:string; lastname:string; email:string }
 
     let private conn (dbname:string) = 
         let c = new SQLiteConnection(sprintf "Data Source=%s.sqlite" dbname)
@@ -15,7 +18,8 @@ module Data =
         let db = conn dbname
         Database.query db "SELECT id, firstname, lastname, email FROM contacts" None 
 
-    let insert contact =
+    let insert c =
         let db = conn dbname
-        let sql = "INSERT INTO contacts (id, firstname, lastname, email) VALUES (@Id, @Firstname, @Lastname, @Email);"
-        Database.execute db sql contact   
+        let entity = { id = c.Id.ToString(); firstname = c.Firstname; lastname = c.Lastname; email = c.Email }
+        let sql = "INSERT INTO contacts (id, firstname, lastname, email) VALUES (@id, @firstname, @lastname, @email);"
+        Database.execute db sql entity   
