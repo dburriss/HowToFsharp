6ea92b48e98a20ad3af9fb3666b60c2e74220864
dburriss <devon@chimplab.co>
2018-11-21 22:12:17 +0100

    Move Input

---
+++ Input.fs
@@ -0,0 +1,54 @@
+namespace Contacts
+
+module Input = 
+
+    open System
+    
+    let captureInput(label:string) = 
+            printf "%s" label
+            Console.ReadLine()
+
+    let captureContact() =
+        {
+            Id = Guid.NewGuid()
+            Firstname = captureInput "First name: "
+            Lastname = captureInput "Last name: "
+            Email = captureInput "Email: "
+        }
+
+    let captureContactChoice saveContact =
+        let contact = captureContact()
+        saveContact contact
+        let another = captureInput "Continue (Y/N)?"
+        match another.ToUpper() with
+        | "Y" -> Choice1Of2 ()
+        | _ -> Choice2Of2 ()
+
+    let rec captureContacts saveContact =
+        match captureContactChoice saveContact with
+        | Choice1Of2 _ -> 
+            captureContacts saveContact
+        | Choice2Of2 _ -> ()
+
+    let printMenu() =
+        printfn "===================="
+        printfn "MENU"
+        printfn "===================="
+        printfn "1. Print Contacts"
+        printfn "2. Capture Contacts"
+        printfn "0. Quit"
+
+    let saveContact contact = printfn "%A" contact
+
+    let routeMenuOption i getContacts saveContact =
+        match i with
+        | "1" -> 
+            printfn "Contacts"
+            getContacts() |> List.iter (printfn "%A")
+        | "2" -> captureContacts saveContact
+        | _ -> printMenu()
+
+    let readKey() =
+        let k = Console.ReadKey()
+        Console.WriteLine()
+        k.KeyChar |> string
--- Program.fs
+++ Program.fs
@@ -1,59 +1,26 @@
-﻿open System
-open Contacts
+﻿open Contacts
+open Contacts.Input
 
-let captureInput(label:string) = 
-        Console.Write(label)
-        Console.ReadLine()
+let getContacts() =
+    Data.all()
+    |> fun r -> match r with
+                | Ok cs -> cs |> Seq.toList
+                | Error e -> 
+                        printfn "ERROR: %s" e.Message
+                        List.empty
 
-let captureContact() =
-    {
-        Id = Guid.NewGuid()
-        Firstname = captureInput "First name: "
-        Lastname = captureInput "Last name: "
-        Email = captureInput "Email: "
-    }
-
-let captureContactChoice saveContact =
-    let contact = captureContact()
-    saveContact contact
-    let another = captureInput "Continue (Y/N)?"
-    match another.ToUpper() with
-    | "Y" -> Choice1Of2 ()
-    | _ -> Choice2Of2 ()
-
-let rec captureContacts saveContact =
-    match captureContactChoice saveContact with
-    | Choice1Of2 _ -> 
-        captureContacts saveContact
-    | Choice2Of2 _ -> ()
-
-let printMenu() =
-    Console.WriteLine("====================")
-    Console.WriteLine("MENU")
-    Console.WriteLine("====================")
-    Console.WriteLine("1. Print Contacts")
-    Console.WriteLine("2. Capture Contacts")
-    Console.WriteLine("0. Quit")
-
-let saveContact contact = printfn "%A" contact
-
-let routeMenuOption i =
-    match i with
-    | "1" -> printfn "Contacts"
-    | "2" -> captureContacts saveContact
-    | _ -> printMenu()
-
-let readKey() =
-    let k = Console.ReadKey()
-    Console.WriteLine()
-    k.KeyChar |> string
+let insertContact c =
+    Data.insert c 
+    |> fun r -> match r with
+                | Ok i -> printfn "%i records inserted" i
+                | Error e -> printfn "ERROR: %s" e.Message
 
 [<EntryPoint>]
 let main argv =
     printMenu()
     let mutable selection = readKey()
     while(selection <> "0") do
-        routeMenuOption selection
+        routeMenuOption selection getContacts insertContact 
         printMenu()
         selection <- readKey()
     0
