41864b3eab75dec75e447e1cb976604228baab28
dburriss <devon@chimplab.co>
2018-11-22 04:53:49 +0100

    Maps from entity

--- Data.fs
+++ Data.fs
@@ -1,6 +1,7 @@
 namespace Contacts
 
 open Contacts
+open System
 open System.Data.SQLite
 
 module Data =
@@ -14,9 +15,14 @@ module Data =
 
     let private dbname = "contactsDB"
 
-    let all()  =
+    let all() =
         let db = conn dbname
         Database.query db "SELECT id, firstname, lastname, email FROM contacts" None 
+        |> Result.map
+            (fun ss -> ss 
+                    |> Seq.map (fun c -> {
+                        Id = Guid.Parse(c.id); Firstname = c.firstname; Lastname = c.lastname; Email = c.email
+                        }))
 
     let insert c =
         let db = conn dbname
